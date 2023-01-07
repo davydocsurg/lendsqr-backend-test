7bb9dc48dbac74dc93f95251a0d52991ac34bb23
davydocsurg <davydocsurg@gmail.com>
2023-01-07 03:15:20 +0100

    update transfer funds test

--- src/controllers/__tests__/transferFunds.test.ts
+++ src/controllers/__tests__/transferFunds.test.ts
@@ -1,6 +1,8 @@
 import request from "supertest";
 import app from "../../app";
 
+const transferFunds = jest.fn();
+
 const data = JSON.stringify({
     amount: 45,
     email: "chibueze@gmail.com",
@@ -10,7 +12,11 @@ const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjcyOTE2MDAwLCJleHAiOjE2NzMwMDI0MDB9.qqAce1s0jQv08KuAutjNMkv_mAyYdAgIZUgaQwoz0vY";
 
 describe("POST /api/transfer-funds", () => {
-    it("should transfer funds to selected user account and return a message", async () => {
+    beforeEach(() => {
+        transferFunds.mockClear();
+    });
+
+    test("should transfer funds to selected user account and return a message", async () => {
         const response = await request(app)
             .post("/api/transfer-funds")
             .set({
@@ -19,7 +25,34 @@ describe("POST /api/transfer-funds", () => {
             })
             .send(data);
 
+        expect(transferFunds).toHaveBeenCalledTimes(1);
         expect(response.status).toBe(200);
-        return expect(response.body).toHaveProperty("message");
+        expect(response.body).toHaveProperty("message");
+    });
+
+    test("should return an error if user is not authenticated", async () => {
+        const response = await request(app)
+            .post("/api/transfer-funds")
+            .set({
+                Accept: "application/json",
+            })
+            .send(data);
+
+        expect(transferFunds).toHaveBeenCalledTimes(0);
+        expect(response.status).toBe(401);
+        expect(response.body).toHaveProperty("error");
+    });
+
+    test("should return an error if amount or email is not provided", async () => {
+        const response = await request(app)
+            .post("/api/transfer-funds")
+            .set({
+                Accept: "application/json",
+            })
+            .send(data);
+
+        expect(transferFunds).toHaveBeenCalledTimes(0);
+        expect(response.status).toBe(401);
+        expect(response.body).toHaveProperty("error");
     });
 });
