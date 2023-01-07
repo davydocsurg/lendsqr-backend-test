a9cc9389b84d67712788432c59736f3187d9b990
davydocsurg <davydocsurg@gmail.com>
2023-01-07 03:13:30 +0100

    update fund wallet test

--- src/controllers/__tests__/fundWallet.test.ts
+++ src/controllers/__tests__/fundWallet.test.ts
@@ -3,6 +3,8 @@ import app from "../../app";
 
 jest.setTimeout(50000);
 
+const fundWallet = jest.fn();
+
 const data = JSON.stringify({
     amount: 20,
 });
@@ -11,7 +13,11 @@ const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjcyOTE2MDAwLCJleHAiOjE2NzMwMDI0MDB9.qqAce1s0jQv08KuAutjNMkv_mAyYdAgIZUgaQwoz0vY";
 
 describe("POST /api/fund-my-wallet", () => {
-    it("funds user account and returns a message", async () => {
+    beforeEach(() => {
+        fundWallet.mockClear();
+    });
+
+    test("should fund user wallet and returns a message", async () => {
         const response = await request(app)
             .post("/api/fund-my-wallet")
             .set({
@@ -20,7 +26,34 @@ describe("POST /api/fund-my-wallet", () => {
             })
             .send(data);
 
+        expect(fundWallet).toHaveBeenCalledTimes(1);
         expect(response.status).toBe(200);
-        return expect(response.body).toHaveProperty("message");
+        expect(response.body).toHaveProperty("message");
+    });
+
+    test("should return an error if user is not authenticated", async () => {
+        const response = await request(app)
+            .post("/api/fund-my-wallet")
+            .set({
+                Accept: "application/json",
+            })
+            .send(data);
+
+        expect(fundWallet).toHaveBeenCalledTimes(0);
+        expect(response.status).toBe(401);
+        expect(response.body).toHaveProperty("error");
+    });
+
+    test("should return an error amount is not provided", async () => {
+        const response = await request(app)
+            .post("/api/fund-my-wallet")
+            .set({
+                Accept: "application/json",
+            })
+            .send(data);
+
+        expect(fundWallet).toHaveBeenCalledTimes(0);
+        expect(response.status).toBe(401);
+        expect(response.body).toHaveProperty("error");
     });
 });
